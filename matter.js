14f8b85b48b637602cc70e4251154395d8915414
yoqubjonovdilmuhammad <dilmuhammadyoqubjonov@gmail.com>
2023-09-19 06:07:18 +0500

    masalalar

---
+++ matter.js
@@ -0,0 +1,181 @@
+// 1-masala
+// let r = 5;
+
+// let s = 3.14 * r ** 2;
+
+// console.log(s);
+
+// 2-masala
+// let a = 6;
+
+// let b = 8;
+
+// let s = a * b;
+// console.log(s);
+
+// let p = 2 * (a + b);
+// console.log(p);
+
+// 3-masala
+// let d = 5;
+
+// let p = 3.14;
+
+// let l = p * d;
+// console.log(l);
+
+// let r = d / 2;
+// console.log(r);
+
+// let s = p * r ** 2;
+// console.log(s);
+
+// 4-masala
+// let a = 5;
+
+// let v = a ** 3;
+// console.log(v);
+
+// let s = 6 * a ** 2;
+// console.log(s);
+
+// 5-masala
+// let a = 4;
+// let b = 8;
+// let c = 7;
+
+// let v = a * b * c;
+// console.log(v);
+
+// let s = 2 * (a * b + b * c + a * c);
+// console.log(s);
+
+// 6-masala
+// let a = 11;
+// let b = 21;
+
+// let m = (a + b) / 2;
+// console.log(m);
+
+// 7-masala
+//let a=6;
+
+//let b=20;
+
+//let g = Math.sqrt(a*b);
+
+//console.log(g);
+
+// 8-masala
+// let a = 5;
+// let b = 9;
+// let c = 12;
+
+// let s = Math.sqrt(a ** 2 + b ** 2);
+// console.log(c);
+
+// let p = a + b + c;
+// console.log(p);
+
+// 9-masala
+// let p = 3.14;
+// let r1 = 6;
+// let r2 = 4;
+
+// let s1 = p * r1;
+// console.log(s1);
+
+// let s2 = p * r2;
+// console.log(s2);
+
+// let s = p * (r1 ** 2 - r2 ** 2);
+// console.log(s);
+
+// 10-masala
+// let p = 3.14;
+// let l = 32;
+
+// let r = l / (3.14 * 2);
+// console.log(r);
+
+// let s = r ** 2 * p;
+// console.log(s);
+
+// 11-masala
+// let x1 = 3;
+// let y1 = 7;
+// let x2 = 5;
+// let y2 = 9;
+
+// let l = Math.sqrt((x1 + y1) ** 2 + (x2 + y2) ** 2);
+// console.log(l);
+
+// 12-masala
+// let x = 6;
+// let y = 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+// console.log(y);
+
+// 13-masala
+// let a = 2;
+
+// let d = a ** 3;
+// console.log(d);
+
+// let d2 = a ** 6;
+// console.log(d2);
+
+// let d3 = a ** 9;
+// console.log(d3);
+
+// 14-masala
+// let f = 12;
+
+// let tf = ((f - 32) * 5) / 9;
+// console.log(tf);
+
+// 15-masala
+// let x = 8;
+
+// let y = 3 * x ** 6 - 6 * x ** 2 - 7;
+// console.log(y);
+
+//16-masala
+
+// let a = 7;
+
+// let b = 5;
+
+// let s1 = b;
+// let s2 = a;
+
+// console.log(s1);
+// console.log(s2);
+
+//17-masala
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+// let s1 = c;
+// let s2 = a;
+// let s3 = b;
+
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+
+//18-masala
+
+// let a = 150;
+
+// let s = a / 10;
+// console.log(s);
+
+//19-masala
+// let a = 1240;
+
+// let s = a / 100;
+
+// let c = s % 10;
+
+// console.log(c);
