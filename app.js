98f5eb688dc743944ee07ffc05ad69c88997a843
Nozim4ik <oripovnozimjon@gmail.com>
2023-09-23 14:31:39 +0500

    guess my number

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
- let pcNum = Math.trunc(Math.random() * 5) + 1;
- let userNum = +prompt(`Enter your number between 1 && 5`);
- if (userNum > 5) {
-   document.write(`Katta son kiritdiz !!!`);
- } else if (userNum < 1) {
-   document.write(`Kichik  son kiritdiz !!!`);
- } else if (userNum == pcNum) {
-   document.write(`Ha bu ${pcNum} edi :)`);
- } else {
-   document.write(`Afsus bu raqam ${pcNum} edi
-      siz esa ${userNum} kiritindiz :(`);
- }
+let pcNum = Math.trunc(Math.random() * 5) + 1;
+let userNum = +prompt(`Enter your number between 1 && 5`);
+if (userNum > 5) {
+  document.write(`Katta son kiritdingiz !`);
+} else if (userNum < 1) {
+  document.write(`Kichik  son kiritdingiz !`);
+} else if (userNum == pcNum) {
+  document.write(`Ha bu raqam ${pcNum} edi.`);
+} else {
+  document.write(`Afsus bu raqam ${pcNum} edi
+      siz esa ${userNum} kiritdingiz.`);
+}
